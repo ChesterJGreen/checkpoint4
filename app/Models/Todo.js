666c06dc28fae32b1e606f7816aff43a74abb944
cjgreenie <seejarom@gmail.com>
2021-07-25 12:18:43 -0600

    todo list polished up

--- app/Models/Todo.js
+++ app/Models/Todo.js
@@ -1,6 +1,6 @@
 import { ProxyState } from "../AppState.js";
 export default class Todo {
-  constructor({ id, completed, user, description }) {
+  constructor({ id, completed = false, user, description }) {
     this.id = id
     //string, required:true, unique: true
     this.completed = completed
@@ -15,7 +15,12 @@ export default class Todo {
 
   get Template() {
     return `
-    <span>'${this.description}'</span>
+    <div class="form-check">
+  <input class="form-check-input todos" type="checkbox" value="${this.completed}">
+  <label class="form-check-label todos" for="todo-item">
+  <span>${this.description}</span><i class="fa fa-trash" aria-hidden="true"></i>
+  </label>
+</div>
   
   `
   }
